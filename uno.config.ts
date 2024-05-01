5babe4127b035ffec90c3c864100633261b10ec8
吴越 <wuyue19970930@gmail.com>
2024-05-01 13:26:12 +0800

    移除 transformer-attributify-jsx

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig, presetAttributify, presetUno } from 'unocss'
-import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx'
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
-  ],
-  transformers: [
-    transformerAttributifyJsx(),
   ],
 })
